536a4ce237f8aa6015f7eb783c06f4604f7fb3c6
Mec <82681280+littleyetz@users.noreply.github.com>
2022-04-26 17:29:21 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const port = 8000;
 
 //rota
-app.get('/', (req , res) =>{
+app.get('/', ( req , res) => {
     res.send('ok!')
 
 })
