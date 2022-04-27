72f4cc501f71ab27d6d3bd759b4b02c244ea7de2
Mec <82681280+littleyetz@users.noreply.github.com>
2022-04-27 14:48:25 -0300

    Add files via upload

--- api.js
+++ api.js
@@ -4,4 +4,12 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
+
+app.get('/', (req, res) => {
+    res.json({
+        msg: 'OK'
+    })
+})
+
+
 app.listen(port);
